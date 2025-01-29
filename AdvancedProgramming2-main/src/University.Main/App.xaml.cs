e135fdfea15985e6cf9b44e70a1e978c34e4169e
Pianoplayre <pianoplayre@gmail.com>
2025-01-29 19:42:25 +0100

    afesjod

---
+++ AdvancedProgramming2-main/src/University.Main/App.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+using System.Windows.Threading;
+using University.Data;
+using University.Interfaces;
+using University.Services;
+using University.ViewModels;
+
+namespace University.Main
+{
+    public partial class App : Application
+    {
+        public IServiceProvider ServiceProvider { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            // Load data from JSON file if it exists
+            var dbContext = ServiceProvider.GetService<UniversityContext>();
+            if (dbContext != null)
+            {
+                dbContext.LoadData("data.json");
+            }
+
+            MainWindow mainWindow = ServiceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+
+            // Register the OnProcessExit event handler
+            AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+
+        }
+
+        private void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<UniversityContext>(options =>
+            {
+                options.UseInMemoryDatabase("UniversityDb");
+                options.UseLazyLoadingProxies();
+            });
+            serviceCollection.AddSingleton<IDialogService, DialogService>();
+            serviceCollection.AddSingleton<IAnimalService, AnimalService>();
+            serviceCollection.AddSingleton<MainWindowViewModel>();
+            serviceCollection.AddSingleton<MainWindow>();
+        }
+
+        private void ApplicationDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.InnerException, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
+
+        private void OnProcessExit(object? sender, EventArgs e)
+        {
+            // Retrieve the UniversityContext from the service provider
+            var dbContext = ServiceProvider.GetService<UniversityContext>();
+            if (dbContext != null)
+            {
+                // Save data to JSON file on application exit
+                dbContext.SaveData("data.json");
+            }
+        }
+
+    }
+}
---
+++ AdvancedProgramming2-main/src/University.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using University.Interfaces;
+using University.Data;
+
+namespace University.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private readonly UniversityContext _context;
+    private readonly IDialogService _dialogService;
+
+    private int _selectedTab;
+    public int SelectedTab
+    {
+        get
+        {
+            return _selectedTab;
+        }
+        set
+        {
+            _selectedTab = value;
+            OnPropertyChanged(nameof(SelectedTab));
+        }
+    }
+
+    private object? _studentsSubView = null;
+    public object? StudentsSubView
+    {
+        get
+        {
+            return _studentsSubView;
+        }
+        set
+        {
+            _studentsSubView = value;
+            OnPropertyChanged(nameof(StudentsSubView));
+        }
+    }
+
+    private object? _animalsSubView = null;
+    public object? AnimalsSubView
+    {
+        get
+        {
+            return _animalsSubView;
+        }
+        set
+        {
+            _animalsSubView = value;
+            OnPropertyChanged(nameof(AnimalsSubView));
+        }
+    }
+
+    private object? _subjectsSubView = null;
+    public object? SubjectsSubView
+    {
+        get
+        {
+            return _subjectsSubView;
+        }
+        set
+        {
+            _subjectsSubView = value;
+            OnPropertyChanged(nameof(SubjectsSubView));
+        }
+    }
+
+    private object? _searchSubView = null;
+    public object? SearchSubView
+    {
+        get
+        {
+            return _searchSubView;
+        }
+        set
+        {
+            _searchSubView = value;
+            OnPropertyChanged(nameof(SearchSubView));
+        }
+    }
+
+    private static MainWindowViewModel? _instance = null;
+    public static MainWindowViewModel? Instance()
+    {
+        return _instance;
+    }
+
+    public MainWindowViewModel(
+        UniversityContext context, 
+        IDialogService dialogService,
+        IAnimalService animalService)
+    {
+        _context = context;
+        _dialogService = dialogService;
+
+        if (_instance is null)
+        {
+            _instance = this;
+        }
+
+        StudentsSubView = new StudentsViewModel(_context, _dialogService);
+        SubjectsSubView = new SubjectsViewModel(_context, _dialogService);
+        AnimalsSubView = new AnimalsViewModel(_context, _dialogService, animalService);
+        SearchSubView = new SearchViewModel(_context, _dialogService);
+    }
+}
